4a904f3a8cc1a7dab79ed13244119a720e8ec2e6
Paulina Cole <lina.cole@gmail.com>
2022-05-21 17:57:11 -0400

    updated server file

--- server.js
+++ server.js
@@ -1,69 +1,17 @@
-const Sequelize = require('sequelize');
-const { STRING, DATE } = Sequelize;
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/clients_instructors_sessions');
+const express = require('express');
+const app = express();
+const {setupDB, Client, Instructor, Session} = require('./db');
 
-const Client = conn.define('client', {
-    name: {
-        type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
+const init = async() => {
+    try {
+        await setupDB();
+        console.log('database is set up')
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`listening on port ${port}`));
     }
-});
-
-const Instructor = conn.define('instructor', {
-    name: {
-        type: STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
+    catch(ex) {
+        console.log(ex)
     }
-});
-
-const Session = conn.define('session', {
-    clientId: {
-        type: STRING,
-        allowNull: false
-    },
-    instructorId: {
-        type: STRING,
-        allowNull: false
-    },
-    sessionDate : {
-        type: DATE,
-        allowNull:  false
-    }
-});
-
-Session.belongsTo(Client);
-Session.belongsTo(Instructor);
-
-const setupDB = async() => {
-    await conn.sync({force: true});
-    const [gleb, catherine, karin, danielle, rahima, demetrius, rodney, melissa] = await Promise.all([
-        Client.create({name: 'gleb'}),
-        Client.create({name: 'catherine'}),
-        Client.create({name: 'karin'}),
-        Client.create({name: 'danielle'}),
-        Instructor.create({name: 'rahima'}),
-        Instructor.create({name: 'demetrius'}),
-        Instructor.create({name: 'rodney'}),
-        Instructor.create({name: 'melissa'}),
-    ]);
-
-    await Promise.all([
-        Session.create({clientId: gleb.id, instructorId: rahima.id, sessionDate: '05/26/22'}),
-        Session.create({clientId: catherine.id, instructorId: demetrius.id, sessionDate: '06/12/22'}),
-        Session.create({clientId: karin.id, instructorId: rodney.id, sessionDate: '05/30/22'}),
-        Session.create({clientId: danielle.id, instructorId: melissa.id, sessionDate: '05/29/22'})
-    ]);
 };
 
-module.exports = {
-    setupDB,
-    Client,
-    Instructor,
-    Session
-};
+init();
